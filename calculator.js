0ec7f99ef1e917476027a3965ccdd5245fe45220
Seun Oshitade <seun.oshitade@gmail.com>
2021-12-31 11:47:17 +0000

    Complete bmiCalculator App

--- calculator.js
+++ calculator.js
@@ -9,9 +9,24 @@ app.get("/", (req, res) => {
 })
 
 app.post("/", (req, res) => {
-  res.send("Thank you for posting that!");
+  const num1 = Number(req.body.num1);
+  const num2 = Number(req.body.num2);
+  const result = num1 + num2;
+  res.send("The result of the calculation is " + result);
 })
 
 app.listen(port, () => {
-  console.log("Server running on port 3000!")
+  console.log("Server running on port 3000!");
+})
+
+//BMI Calculator
+app.get("/bmicalculator", (req, res) => {
+  res.sendFile(__dirname + "/bmiCalculator.html");
+})
+
+app.post("/bmicalculator", (req, res) => {
+  const wgt = Number(req.body.weight);
+  const hgt = Number(req.body.height);
+  const bmi = (wgt / hgt**2).toFixed(2);
+  res.send(`<h2>Your BMI is ${bmi} Kg/Sqm.</h2>`)
 })
